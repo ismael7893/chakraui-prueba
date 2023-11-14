b3735790abe10fa6e3c25cd114358da60ed5db29
Gustavo Aquino <gtv7893@gmail.com>
2023-11-14 13:01:50 -0500

    ini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import {
+  Flex,
+  Box,
+  Spacer,
+  Text,
+  Link,
+  List,
+  ListItem,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton
+} from '@chakra-ui/react';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex bg="blue.500" p="4" color="white">
+        <Box>
+          <strong>Prueba</strong>
+        </Box>
+        <Spacer />
+      </Flex>
+
+      {/* Main Content */}
+      <Flex>
+        {/* Sidebar */}
+        <Box w="250px" bg="gray.200" p="4">
+          <Text fontSize="xl" mb="4">
+            Menu
+          </Text>
+          <List spacing={3}>
+            <ListItem>
+              <Link color="blue.500">Menu 1</Link>
+            </ListItem>
+            <ListItem>
+              <Link color="blue.500">Menu 2</Link>
+            </ListItem>
+            <ListItem>
+              <Link color="blue.500">Menu 3</Link>
+            </ListItem>
+          </List>
+        </Box>
+        <Box flex="1" p="4">
+          <Text fontSize="xl" mb="4" textAlign={'left'}>
+            Dashboard Overview
+          </Text>
+          <Box bg="white" p="4" borderRadius="md" boxShadow="md">
+            <Button mt="4" colorScheme="teal" onClick={handleOpenModal}>
+              Abrir un modal
+            </Button>
+
+            {/* Modal */}
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>titulo de Modal</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Text>Contenido de prueba del modal</Text>
+                </ModalBody>
+                <ModalFooter>
+                  <Button colorScheme="teal" mr={3} onClick={handleCloseModal}>
+                    Cerrar
+                  </Button>
+                  {/* Add additional modal actions as needed */}
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Box>
+        </Box>
+      </Flex>
     </div>
   );
 }
